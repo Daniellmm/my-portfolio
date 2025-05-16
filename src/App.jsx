e5cb5c73b6cead6ab7aaeeee43758d9264bdb9cd
Daniellmm <idowudanielcsc190400@gmail.com>
2025-05-16 12:56:34 -1100

    removed the 3d for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import LogoSection from "./sections/LogoSection"
 import NavBar from "./components/NavBar"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
@@ -9,6 +11,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
     </>
   )
 }
--- src/test.jsx
+++ src/test.jsx
@@ -1,39 +1,12 @@
-import React from 'react'
-import { words } from '../constants'
-import Button from '../components/Button'
-import HeroExperience from '../components/HeroModels/HeroExperience'
-import {useGSAP} from '@gsap/react'
-import { gsap } from 'gsap'
-import AnimatedCounter from '../components/AnimatedCounter'
-
-
-const Hero = () => {
-
-  useGSAP(() => {
-    gsap.fromTo('.hero-text h1',
-      {
-        y: 50,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        stagger: 0.2,
-        duration: 1,
-        ease: 'power2.inOut',
-      }
-    )
-  })
-  return (
-    <section id='hero' className=' relative overflow-hidden'>
+{/* <section id='hero' className=' relative overflow-hidden'>
       <div className=' absolute top-0 left-0 z-10'>
         <img src="/images/bg.png" alt="backgroung" />
       </div>
 
       <div className='hero-layout'>
-        {/* left side for the text  */}
-        <header className='flex flex-col justify-center items-center md:w-full w-screen md:px-20 px-5'>
-          <div className='flex flex-col gap-7'>
+        {/* left side for the text  
+        <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+          <div className='flex  flex-col gap-7'>
             <div className='hero-text'>
               <h1>Shaping
                 <span className='slide'>
@@ -67,7 +40,7 @@ const Hero = () => {
               text="See my Work"
             />
           </div>
-        </header>
+        </header> */}
 
         {/* right side for the 3d model */}
         {/* <figure>
@@ -76,12 +49,8 @@ const Hero = () => {
           </div>
         </figure> */}
 
-        {/* i think for now i will add anothen thing here */}
+        {/* i think for now i will add anothen thing here 
         
       </div>
       <AnimatedCounter />
-    </section>
-  )
-}
-
-export default Hero
+    </section> */}
